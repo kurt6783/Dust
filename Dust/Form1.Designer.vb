a3e6b747c35ef2be1dde3e8d4f0fa100b4e8420d
kurt6783 <k0911245920@gmail.com>
2020-03-11 14:22:08 +0800

    First

---
+++ Dust/Form1.Designer.vb
@@ -0,0 +1,208 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Form1
+    Inherits System.Windows.Forms.Form
+
+    'Form 覆寫 Dispose 以清除元件清單。
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    '為 Windows Form 設計工具的必要項
+    Private components As System.ComponentModel.IContainer
+
+    '注意: 以下為 Windows Form 設計工具所需的程序
+    '可以使用 Windows Form 設計工具進行修改。
+    '請不要使用程式碼編輯器進行修改。
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.components = New System.ComponentModel.Container
+        Me.ComboBox1 = New System.Windows.Forms.ComboBox
+        Me.ComboBox2 = New System.Windows.Forms.ComboBox
+        Me.ComboBox3 = New System.Windows.Forms.ComboBox
+        Me.ComboBox4 = New System.Windows.Forms.ComboBox
+        Me.ComboBox5 = New System.Windows.Forms.ComboBox
+        Me.Button1 = New System.Windows.Forms.Button
+        Me.SerialPort1 = New System.IO.Ports.SerialPort(Me.components)
+        Me.ComboBox6 = New System.Windows.Forms.ComboBox
+        Me.Button2 = New System.Windows.Forms.Button
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox
+        Me.Button4 = New System.Windows.Forms.Button
+        Me.Button3 = New System.Windows.Forms.Button
+        Me.GroupBox2 = New System.Windows.Forms.GroupBox
+        Me.ListBox1 = New System.Windows.Forms.ListBox
+        Me.GroupBox1.SuspendLayout()
+        Me.GroupBox2.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'ComboBox1
+        '
+        Me.ComboBox1.FormattingEnabled = True
+        Me.ComboBox1.Location = New System.Drawing.Point(6, 17)
+        Me.ComboBox1.Name = "ComboBox1"
+        Me.ComboBox1.Size = New System.Drawing.Size(67, 20)
+        Me.ComboBox1.TabIndex = 0
+        Me.ComboBox1.Text = "COM3"
+        '
+        'ComboBox2
+        '
+        Me.ComboBox2.Enabled = False
+        Me.ComboBox2.FormattingEnabled = True
+        Me.ComboBox2.Location = New System.Drawing.Point(136, 17)
+        Me.ComboBox2.Name = "ComboBox2"
+        Me.ComboBox2.Size = New System.Drawing.Size(67, 20)
+        Me.ComboBox2.TabIndex = 1
+        Me.ComboBox2.Text = "9600"
+        '
+        'ComboBox3
+        '
+        Me.ComboBox3.Enabled = False
+        Me.ComboBox3.FormattingEnabled = True
+        Me.ComboBox3.Location = New System.Drawing.Point(266, 17)
+        Me.ComboBox3.Name = "ComboBox3"
+        Me.ComboBox3.Size = New System.Drawing.Size(67, 20)
+        Me.ComboBox3.TabIndex = 2
+        Me.ComboBox3.Text = "N"
+        '
+        'ComboBox4
+        '
+        Me.ComboBox4.Enabled = False
+        Me.ComboBox4.FormattingEnabled = True
+        Me.ComboBox4.Location = New System.Drawing.Point(396, 17)
+        Me.ComboBox4.Name = "ComboBox4"
+        Me.ComboBox4.Size = New System.Drawing.Size(67, 20)
+        Me.ComboBox4.TabIndex = 3
+        Me.ComboBox4.Text = "8"
+        '
+        'ComboBox5
+        '
+        Me.ComboBox5.Enabled = False
+        Me.ComboBox5.FormattingEnabled = True
+        Me.ComboBox5.Location = New System.Drawing.Point(526, 17)
+        Me.ComboBox5.Name = "ComboBox5"
+        Me.ComboBox5.Size = New System.Drawing.Size(67, 20)
+        Me.ComboBox5.TabIndex = 4
+        Me.ComboBox5.Text = "1"
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(656, 17)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(66, 20)
+        Me.Button1.TabIndex = 5
+        Me.Button1.Text = "連線"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'SerialPort1
+        '
+        '
+        'ComboBox6
+        '
+        Me.ComboBox6.FormattingEnabled = True
+        Me.ComboBox6.Items.AddRange(New Object() {"CAM房", "FQC", "內層", "外層", "印刷(UCoater)", "印刷", "綠漆", "線路", "壓合一廠疊板房", "壓合二廠疊板房", ""})
+        Me.ComboBox6.Location = New System.Drawing.Point(6, 21)
+        Me.ComboBox6.Name = "ComboBox6"
+        Me.ComboBox6.Size = New System.Drawing.Size(587, 20)
+        Me.ComboBox6.TabIndex = 7
+        '
+        'Button2
+        '
+        Me.Button2.Location = New System.Drawing.Point(656, 21)
+        Me.Button2.Name = "Button2"
+        Me.Button2.Size = New System.Drawing.Size(195, 19)
+        Me.Button2.TabIndex = 8
+        Me.Button2.Text = "資料轉換"
+        Me.Button2.UseVisualStyleBackColor = True
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.ListBox1)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.ComboBox1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox2)
+        Me.GroupBox1.Controls.Add(Me.ComboBox3)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.ComboBox4)
+        Me.GroupBox1.Controls.Add(Me.ComboBox5)
+        Me.GroupBox1.Location = New System.Drawing.Point(12, 12)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(857, 269)
+        Me.GroupBox1.TabIndex = 9
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "落塵蒐集"
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(783, 43)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(66, 219)
+        Me.Button4.TabIndex = 9
+        Me.Button4.Text = "刪除"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(783, 17)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(66, 20)
+        Me.Button3.TabIndex = 7
+        Me.Button3.Text = "斷線"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'GroupBox2
+        '
+        Me.GroupBox2.Controls.Add(Me.ComboBox6)
+        Me.GroupBox2.Controls.Add(Me.Button2)
+        Me.GroupBox2.Location = New System.Drawing.Point(12, 287)
+        Me.GroupBox2.Name = "GroupBox2"
+        Me.GroupBox2.Size = New System.Drawing.Size(857, 55)
+        Me.GroupBox2.TabIndex = 10
+        Me.GroupBox2.TabStop = False
+        Me.GroupBox2.Text = "Excel轉換"
+        '
+        'ListBox1
+        '
+        Me.ListBox1.FormattingEnabled = True
+        Me.ListBox1.ItemHeight = 12
+        Me.ListBox1.Location = New System.Drawing.Point(6, 42)
+        Me.ListBox1.Name = "ListBox1"
+        Me.ListBox1.Size = New System.Drawing.Size(771, 220)
+        Me.ListBox1.TabIndex = 10
+        '
+        'Form1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 12.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(881, 354)
+        Me.Controls.Add(Me.GroupBox2)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Form1"
+        Me.Text = "落塵量計算"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox2.ResumeLayout(False)
+        Me.ResumeLayout(False)
+
+    End Sub
+    Friend WithEvents ComboBox1 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox2 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox3 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox4 As System.Windows.Forms.ComboBox
+    Friend WithEvents ComboBox5 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button1 As System.Windows.Forms.Button
+    Friend WithEvents SerialPort1 As System.IO.Ports.SerialPort
+    Friend WithEvents ComboBox6 As System.Windows.Forms.ComboBox
+    Friend WithEvents Button2 As System.Windows.Forms.Button
+    Friend WithEvents GroupBox1 As System.Windows.Forms.GroupBox
+    Friend WithEvents GroupBox2 As System.Windows.Forms.GroupBox
+    Friend WithEvents Button3 As System.Windows.Forms.Button
+    Friend WithEvents Button4 As System.Windows.Forms.Button
+    Friend WithEvents ListBox1 As System.Windows.Forms.ListBox
+
+End Class
---
+++ Dust/Form1.vb
@@ -0,0 +1,201 @@
+﻿Imports System.IO.Ports
+Imports System.Threading
+Imports Excel = Microsoft.Office.Interop.Excel
+Imports System.Runtime.InteropServices
+Imports Microsoft.Office.Interop
+
+Public Class Form1
+    Dim Creat As String
+    Dim MyPort As Array
+    Dim ReceiveData As String
+    Dim File As System.IO.StreamWriter
+    Dim Result As String
+    Dim Sn As Integer
+    Dim StartDate As DateTime
+    Dim StartTime As String
+
+    Private Sub Form1_Load(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles MyBase.Load
+        Button1.Enabled = True
+        Button3.Enabled = False
+        Sn = 1
+        StartDate = DateTime.Now
+        StartTime = StartDate.ToString("yyyyMMdd_HHmmss")
+        MyPort = IO.Ports.SerialPort.GetPortNames()
+        ComboBox1.Items.AddRange(MyPort)
+        Creat = "Sn" & "," & "Date" & "," & "Time" & "," & "Sigma0.3" & "," & "Sigma0.5" & "," & "Sigma1.0" & "," & "Sigma3.0" & "," & "Sigma5.0" & "," & "Sigma10.0" & "," & "Delta0.3" & "," & "Delta0.5" & "," & "Delta1.0" & "," & "Delta3.0" & "," & "Delta5.0" & "," & "Delta10.0"
+        Log(Creat)
+    End Sub
+
+    Private Sub Button1_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button1.Click
+        SerialPort1.PortName = ComboBox1.Text
+        SerialPort1.BaudRate = ComboBox2.Text
+        SerialPort1.Parity = Parity.None
+        SerialPort1.DataBits = 8
+        SerialPort1.StopBits = StopBits.One
+        SerialPort1.Open()
+        If SerialPort1.IsOpen = True Then
+            Button1.Enabled = False
+            Button3.Enabled = True
+        ElseIf SerialPort1.IsOpen = False Then
+            Button1.Enabled = True
+            Button3.Enabled = False
+            MsgBox("連線失敗，請重試。")
+        End If
+    End Sub
+
+    Sub Log(ByVal Result As String)
+        ListBox1.Items.Add(Result)
+    End Sub
+
+    Private Sub SerialPort1_DataReceived(ByVal sender As Object, ByVal e As System.IO.Ports.SerialDataReceivedEventArgs) Handles SerialPort1.DataReceived
+        Dim DataStr As String
+        Dim DataArray(100) As String
+        Dim Total_03 As Integer
+        Dim Total_05 As Integer
+        Dim Total_10 As Integer
+        Dim Total_30 As Integer
+        Dim Total_50 As Integer
+        Dim Total_100 As Integer
+        Thread.Sleep(500)
+        DataStr = SerialPort1.ReadExisting
+        DataArray = DataStr.Split(",")
+        Total_100 = Val(DataArray(27))
+        Total_50 = Total_100 + Val(DataArray(25))
+        Total_30 = Total_50 + Val(DataArray(23))
+        Total_10 = Total_30 + Val(DataArray(21))
+        Total_05 = Total_10 + Val(DataArray(19))
+        Total_03 = Total_05 + Val(DataArray(17))
+        Result = Sn & "," & DataArray(0).TrimStart & "," & DataArray(1) & "," & Total_03.ToString & "," & Total_05.ToString & "," & Total_10.ToString & "," & Total_30.ToString & "," & Total_50.ToString & "," & Total_100.ToString & "," & DataArray(17) & "," & DataArray(19) & "," & DataArray(21) & "," & DataArray(23) & "," & DataArray(25)
+        Log(Result)
+        Sn = Sn + 1
+    End Sub
+
+   
+
+    Private Sub Button3_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button3.Click
+        SerialPort1.Close()
+        If SerialPort1.IsOpen = True Then
+            Button1.Enabled = False
+            Button3.Enabled = True
+        ElseIf SerialPort1.IsOpen = False Then
+            Button1.Enabled = True
+            Button3.Enabled = False
+            MsgBox("連線已中斷。")
+        End If
+    End Sub
+
+    Private Sub Button2_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button2.Click
+        File = My.Computer.FileSystem.OpenTextFileWriter(Application.StartupPath & "\DustData\" & StartTime & ".csv", True)
+        For i = 0 To ListBox1.Items.Count - 1
+            File.WriteLine(ListBox1.Items.Item(i))
+        Next
+        File.Close()
+
+
+        '1.讀取落塵值
+        Button2.Text = "資料轉換中"
+        Dim app As New Excel.Application
+        Dim book As Excel.Workbook
+        Dim sheet As Excel.Worksheet
+        Dim range As Excel.Range
+        app.DisplayAlerts = False
+        app.Visible = False
+        
+        Dim DustData(,) As Integer = Nothing
+        Dim Row As Integer
+        Select Case ComboBox6.Text
+            Case "CAM房"
+                ReDim DustData(7, 1)
+                Row = 8
+            Case "FQC"
+                ReDim DustData(44, 1)
+                Row = 45
+            Case "內層"
+                ReDim DustData(62, 1)
+                Row = 63
+            Case "外層"
+                ReDim DustData(25, 1)
+                Row = 26
+            Case "印刷(UCoater)"
+                ReDim DustData(38, 1)
+                Row = 39
+            Case "印刷"
+                ReDim DustData(34, 1)
+                Row = 35
+            Case "綠漆"
+                ReDim DustData(76, 1)
+                Row = 77
+            Case "線路"
+                ReDim DustData(52, 1)
+                Row = 53
+            Case "壓合一廠疊板房"
+                ReDim DustData(5, 1)
+                Row = 6
+            Case "壓合二廠疊板房"
+                ReDim DustData(11, 1)
+                Row = 12
+        End Select
+
+        book = app.Workbooks.Open(Application.StartupPath & "\DustData\" & StartTime & ".csv")
+        sheet = book.Sheets(1)
+
+        If ComboBox6.Text = "FQC" Or ComboBox6.Text = "線路" Or ComboBox6.Text = "綠漆" Then
+            For i = 0 To Row - 1
+                range = sheet.Cells(i + 2, 5) 'Sigma 0.5
+                DustData(i, 0) = range.Value
+                range = sheet.Cells(i + 2, 8) 'Sigma 5
+                DustData(i, 1) = range.Value
+            Next
+        ElseIf ComboBox6.Text = "CAM房" Or ComboBox6.Text = "內層" Or ComboBox6.Text = "外層" Or ComboBox6.Text = "印刷(UCoater)" Or ComboBox6.Text = "印刷" Or ComboBox6.Text = "壓合一廠疊板房" Or ComboBox6.Text = "壓合二廠疊板房" Then
+            For i = 0 To Row - 1
+                range = sheet.Cells(i + 2, 12) ' Delta 1
+                DustData(i, 0) = range.Value
+                range = sheet.Cells(i + 2, 5) ' Sigma 0.5
+                DustData(i, 1) = range.Value
+            Next
+        End If
+        book.Close()
+        '2.讀取表單設定
+        Dim TXT As New System.IO.StreamReader(Application.StartupPath & "\DustSetting\" & ComboBox6.Text & ".txt", System.Text.Encoding.GetEncoding("BIG5"))
+        Dim DustSetting(Row - 1, 4)
+        Dim Dust(4) As String
+        Dim x As Integer = 0
+        Dim Str As String
+        Str = TXT.ReadLine
+        Do Until Str = Nothing
+            Dust = Str.Split(",")
+            DustSetting(x, 0) = Dust(0) 'Sn
+            DustSetting(x, 1) = Dust(1) '0.5x
+            DustSetting(x, 2) = Dust(2) '0.5y
+            DustSetting(x, 3) = Dust(3) '5x
+            DustSetting(x, 4) = Dust(4) '5y
+            x = x + 1
+            Str = TXT.ReadLine
+        Loop
+        '3.讀取基底表單、填入表單、另存表單
+        book = app.Workbooks.Open(Application.StartupPath & "\DustList\" & ComboBox6.Text & ".xls")
+        sheet = book.Sheets(1)
+        For i = 0 To Row - 1
+            range = sheet.Cells(CInt(DustSetting(i, 2)), CInt(DustSetting(i, 1)))
+            range.Value = DustData(i, 0)
+            range = sheet.Cells(CInt(DustSetting(i, 4)), CInt(DustSetting(i, 3)))
+            range.Value = DustData(i, 1)
+            Button2.Text = "資料轉換中" & "第" & i & "筆"
+        Next
+        book.SaveAs((Application.StartupPath & "\ExcelData\" & ComboBox6.Text & "_" & StartTime & ".xls"))
+        book.Close()
+        app.Quit()
+        System.Runtime.InteropServices.Marshal.ReleaseComObject(sheet)
+        System.Runtime.InteropServices.Marshal.ReleaseComObject(book)
+        System.Runtime.InteropServices.Marshal.ReleaseComObject(app)
+        GC.Collect()
+
+        MsgBox("資料轉換完成。")
+        Button2.Text = "資料轉換"
+    End Sub
+
+    Private Sub Button4_Click(ByVal sender As System.Object, ByVal e As System.EventArgs) Handles Button4.Click
+        ListBox1.Items.RemoveAt(ListBox1.SelectedIndex)
+        Sn = Sn - 1
+    End Sub
+End Class
